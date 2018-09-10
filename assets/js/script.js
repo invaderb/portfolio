415d7a3c3550c6a0a55f2359fdef65a431c0a3c8
Braydon Harris <braydonharris@gmail.com>
2018-09-10 09:02:29 -0600

    - Removing unneeded css file - Starting to add grid layout for portfolio items

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,9 @@ let menuItem = document.getElementsByClassName("drawer-menu--item");
 let isToggled = false;
 burger.style.left = "60px"
 
-function toggle(){
+function toggle() {
     // Look into replacing this with something more supported.
-    if(isToggled){
+    if (isToggled) {
         burger.style.left = "60px"
         toggleClasses();
         isToggled = !isToggled
@@ -28,10 +28,34 @@ function closeNav() {
     return menu.style.width = "50px";
 }
 
-function toggleClasses(){
+function toggleClasses() {
     burger.classList.toggle('is-active');
 
     for (var i = 0; i < menuItem.length; i++) {
         menuItem[i].classList.toggle('is-transition');
     }
+}
+
+function resizeGridItem(item) {
+    grid = document.getElementsByClassName("grid")[0];
+    rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+    rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+    rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+    item.style.gridRowEnd = "span " + rowSpan;
+}
+
+let allItems = document.getElementsByClassName("item");
+
+window.onload = resizeAllGridItems();
+window.addEventListener("resize", resizeAllGridItems);
+
+function resizeAllGridItems() {
+    for (x = 0; x < allItems.length; x++) {
+        resizeGridItem(allItems[x]);
+    }
+}
+
+function resizeInstance(instance) {
+    item = instance.elements[0];
+    resizeGridItem(item);
 }
